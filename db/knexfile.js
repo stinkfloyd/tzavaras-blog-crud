6306a4efb5ef2a5c7f70f140e145c932b367ac8a
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-10-22 17:00:13 -0600

    "Production env added"

--- db/knexfile.js
+++ db/knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: `pg`,
     connection: `postgres://localhost/blogs`
+  },
+  production: {
+    client: `pg`,
+    connection: `postgres://tyrpcaamiymtoh:c62ebd8d2d832c79a808e8629e3dc7886cae9fa82de705101ea8755b1bce7544@ec2-184-72-234-230.compute-1.amazonaws.com:5432/d5pqt0csrlasm1`
   }
 }
